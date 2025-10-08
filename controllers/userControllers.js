e1e29bb7714757c199f16684365e9766ce5bca89
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-08 15:41:26 +0530

    change

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,11 @@
-export const getUsers = (req, res) => {
-  res.send("get all users");
+import User from "../models/users.js";
+export const getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
 };
+
+// export const getUsersID = async (req, res) => {
+//   // const getUsersID = req.params.id;
+//   const user = await user.findByIdAndDelete(req.params.id);
+//   res.json(user);
+// };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ import { getUsers } from "../controllers/userControllers.js";
 const router = express.Router();
 
 router.get("/", getUsers);
+// router.get("/:id", getUsersID);
 
 export default router;
